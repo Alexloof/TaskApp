7e5b5887390ad92964f55bdd328f343949bb1483
Alex Lööf <alex.loofet@gmail.com>
2018-06-26 10:14:20 +0200

    fixed drag bug with translateX on container

--- client/src/views/board/style.js
+++ client/src/views/board/style.js
@@ -1,17 +1,19 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  margin-left: 40px;
-  margin-top: 50px;
-  padding: 30px;
+  /* margin-left: 40px;
+  margin-top: 50px; */
+  position: relative;
+  left: ${({ activeMenu }) => (activeMenu ? '280px' : '50px')};
+  top: 80px;
   display: flex;
-  height: calc(100vh - 50px);
+  height: calc(100vh - 100px);
   width: ${({ activeMenu }) =>
-    activeMenu ? 'calc(100vw - 270px)' : 'calc(100vw - 40px)'};
+    activeMenu ? 'calc(100vw - 280px)' : 'calc(100vw - 50px)'};
   overflow: auto;
   transition: 0.5s all ease;
-  transform: ${({ activeMenu }) =>
-    activeMenu ? 'translateX(230px)' : 'translateX(0px)'};
+  /* transform: ${({ activeMenu }) =>
+    activeMenu ? 'translateX(230px)' : 'translateX(0px)'}; */
 `
 
 export const ListsWrapper = styled.div`
