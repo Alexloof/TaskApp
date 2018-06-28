fd8fbca0220b5240d512a6354bcc6fa966c7db90
Alex Lööf <alex.loofet@gmail.com>
2018-06-28 09:53:45 +0200

    google/fb login setup

--- client/src/views/login/index.js
+++ client/src/views/login/index.js
@@ -9,26 +9,36 @@ import {
   ButtonGroup
 } from './style'
 
-import Icon from '../../components/Icon'
-
 class Login extends Component {
+  googleLogin = () => {
+    console.log('login google')
+    localStorage.setItem('token', 'adadadad')
+    this.props.history.push('/')
+  }
+
+  facebookLogin = () => {
+    console.log('login facebook')
+    localStorage.setItem('token', 'adadadad')
+    this.props.history.push('/')
+  }
+
   render() {
     return (
       <Container>
         <Title>Sign in to get started</Title>
         <SubTitle>
-          TaskApp is a place where you can plan your day, week or month. Or
+          TaskApp is a place where you can plan your day, week or month. Or you
           maybe just want to make tasks. <br /> <br /> Sign in below to get
           started!
         </SubTitle>
         <ButtonGroup>
-          <FacebookBtn>
+          <FacebookBtn onClick={this.facebookLogin}>
             <i
               style={{ color: 'white', marginRight: '15px' }}
               className="fab fa-facebook-f"
             />Login
           </FacebookBtn>
-          <GoogleBtn>
+          <GoogleBtn onClick={this.googleLogin}>
             <i
               style={{ color: 'white', marginRight: '15px' }}
               className="fab fa-google"
